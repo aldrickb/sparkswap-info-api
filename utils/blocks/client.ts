f609df151fe7108623e5a1e57d0dda58a34c87bd
Aldrick Bonaobra <git@aldrickb.com>
2022-01-17 15:10:57 +0800

    Update client.ts

--- utils/blocks/client.ts
+++ utils/blocks/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const blockClient = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "https://api.thegraph.com/subgraphs/name/aldrickb/test2sparkblock",
+    uri: "https://api.thegraph.com/subgraphs/name/dynamic-amm/ethereum-blocks-ethereum",
   }),
   cache: new InMemoryCache(),
 });
